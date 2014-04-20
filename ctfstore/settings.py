531a4d4ce3da4bdd7478e4d205ae4a6cd185eb8b
Daniel Stelter-Gliese <ds@siriuswhite.de>
2014-04-20 22:14:49 +0200

    Highlight current navigation pane

--- ctfstore/settings.py
+++ ctfstore/settings.py
@@ -62,7 +62,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    "store.context_processors.isp_branding"
+    "store.context_processors.isp_branding",
+    "store.context_processors.navigation"
 )
 
 ROOT_URLCONF = 'ctfstore.urls'
--- store/context_processors.py
+++ store/context_processors.py
@@ -1,5 +1,17 @@
 from django.conf import settings
+from django.core.urlresolvers import resolve
+
 
 def isp_branding(request):
 	return {'isp_name': settings.ISP_NAME}
 
+def navigation(request):
+	return {'nav_active': resolve(request.path_info).url_name,
+	        'nav_items': [
+	            ('home', 'Übersicht'),
+	            ('achievements', 'Bonusprogramm'),
+	            ('upgrades', 'Tarifoptionen'),
+	            ('contact', 'Kontakt'),
+	            ('career', 'Karriere')
+	        ]
+	}
