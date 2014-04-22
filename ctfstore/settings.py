a341fd8947d0e6df403d55ba2ebf8fa09501e361
Kai Fabian <kai.fabian@student.hpi.uni-potsdam.de>
2014-04-22 14:54:10 +0200

    Finally fixed UTF-8 problems.

--- ctfstore/settings.py
+++ ctfstore/settings.py
@@ -16,6 +16,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+DEFAULT_CHARSET='UTF-8'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
