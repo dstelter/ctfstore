9cbb68238ebf2af58784369f1b0567867f968e15
Kai Fabian <kai.fabian@student.hpi.uni-potsdam.de>
2014-04-22 14:53:56 +0200

    start.sh forced python3

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-uwsgi --socket :8001 --module ctfstore.wsgi >> /var/log/g-online 2>&1
+uwsgi_python3 --socket :8001 --module ctfstore.wsgi >> /var/log/g-online 2>&1
 
- 
